65e3722eb1a451e4650f058367f979f0ccf4259c
Jesse Ward <fmfs33@durham.ac.uk>
2024-12-20 17:43:39 +0000

    mostly done with all the tutorial stuff, need to do week 9 then done

--- hello.js
+++ hello.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function(req, resp){
    resp.send('Hello world')
 })
+app.get('/w', (req, res) => {
+   res.send("hello" + req.query.person)
+ })
+app.post("/new", function(req, resp){
+   console.log("got request")
+   console.log(req.body.person)
+   resp.send("hello you")
+})
+
 
 app.listen(8090)
