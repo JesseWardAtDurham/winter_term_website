d38e406b83e47dfd844e09ce7339b6e0cd262523
Jesse Ward <fmfs33@durham.ac.uk>
2024-12-22 19:43:46 +0000

    did a lot in this commit, functional get and post requests can only add bird pics for now but should be able to add new bird species soon

--- client/index.js
+++ client/index.js
@@ -1,6 +1,98 @@
-window.addEventListener('click', function(event){
-    fetch('http://127.0.0.1:8090/list')
+
+robinButton =  document.getElementById("Robin")
+
+robinButton.addEventListener('click', function(event){
+    fetch('http://127.0.0.1:8090/bird?bird=Robin')
      .then(response => response.text())
      .then(body =>
-        document.getElementById('content').innerHTML=body)
+        displayIMG(body)
+        
+     )
   });
+
+finchButton =  document.getElementById("Goldfinch")
+
+finchButton.addEventListener('click', function(event){
+    fetch('http://127.0.0.1:8090/bird?bird=Goldfinch')
+     .then(response => response.text())
+     .then(body =>
+        displayIMG(body)
+        
+     )
+  });
+
+newPic=document.getElementById("newBirdPicture")
+
+newPic.addEventListener("click",function(event){
+    purgeImages()
+    col=document.getElementsByClassName("col")
+    newForm=document.createElement("form")
+    newForm.action="http://127.0.0.1:8090/newPic"
+    newForm.method="post"
+    p1=document.createElement("p")
+    p1.innerHTML="Bird Name"
+    input1=document.createElement("input")
+    input1.name="birdName"
+    p1.append(input1)
+    newForm.append(p1)
+    p2=document.createElement("p")
+    p2.innerHTML="Picture URL"
+    input2=document.createElement("input")
+    input2.name="pictureURL"
+    p2.append(input2)
+    newForm.append(p2)
+    p3=document.createElement("p")
+    p3.innerHTML="Alt text for your image"
+    input3=document.createElement("input")
+    input3.name="altText"
+    p3.append(input3)
+    newForm.append(p3)
+    p4=document.createElement("p")
+    input4=document.createElement("input")
+    input4.type="submit"
+    p4.append(input4)
+    newForm.append(p4)
+    col[0].append(newForm)
+
+})
+
+
+function purgeImages(){
+    i=0
+    col=document.getElementsByClassName("col")
+    current=col[i].firstElementChild
+    while(col[i]){
+        current=col[i].firstElementChild
+        while(current){
+            temp=current.nextElementSibling
+            current.remove()
+            current=temp
+        }
+        i+=1
+
+    }
+}
+
+function displayIMG(json){
+    purgeImages()
+    jsonData=JSON.parse(json);
+    console.log(jsonData["bird_pictures"])
+    i=0
+    col=document.getElementsByClassName("col")
+    while(jsonData["bird_pictures"][i]!=undefined){
+        img1=document.createElement("img")
+        img1.src=jsonData["bird_pictures"][i]["pictureURL"]
+        img1.className="img-fluid"
+        col[i%2].append("",img1)
+        i+=1
+    }
+}
+// a=document.createElement("div")
+// dartmoorButton.after(a)
+
+// col=document.getElementsByClassName("col")
+// console.log(col[0].firstElementChild.nextElementSibling)
+// k=col[0].firstElementChild.nextElementSibling
+// h=document.createElement("img")
+// h.src="birdPictures/goldfinch-7228334_640.jpg"
+// col[0].append("",h)
--- server.js
+++ server.js
@@ -2,36 +2,66 @@ const express = require('express')
 const app = express()
 
 
-let jsonData = require("./birdsandpics.json"); 
+let jsonData = require("./birdsandpics.json");
 jsonData = JSON.parse(JSON.stringify(jsonData))
 
 app.use("/client", express.static(__dirname + "/client"));
 app.use(express.urlencoded({ extended: false }));
 
 
-app.get('/', function(req, resp){
-   a=jsonData["National_Parks"][0]["bird_pictures"][0]
-   
+app.get('/', function (req, resp) {
+   a = jsonData["National_Parks"][0]["bird_pictures"][0]
+
    resp.send(jsonData["Bird_Pictures"][a])
-   
+
 })
 
 
 app.get('/w', (req, res) => {
    res.send("hello" + req.query.person)
- })
+})
 
 
-app.post("/new", function(req, resp){
+app.post("/new", function (req, resp) {
    console.log("got request")
    console.log(req.body.person)
    console.log(jsonData)
    resp.send("hello you")
 })
 
-app.get('/list', function (req, resp){
-    resp.send(instruments);
+app.post("/newPic",function(req, resp){
+   birdName=req.body.birdName
+   pictureURL=req.body.pictureURL
+   altText=req.body.altText
+   console.log("hello")
+   i=0
+   while(jsonData["Birds"][i]){
+      if(jsonData["Birds"][i]["birdName"]===birdName){
+         jsonData["Birds"][i]["bird_pictures"].push({pictureURL:pictureURL,alt_text_for_image:altText})
+      }
+      i+=1
+   }
+   console.log(jsonData["Birds"])
+   resp.redirect("http://127.0.0.1:8090/client/")
+
 })
 
+app.get('/bird', function (req, resp) {
+   toGo = getBird(req.query.bird)
+   //console.log(toGo)
+   resp.send(toGo);
+})
+
+function getBird(birdName) {
+   currrentData = jsonData["Birds"][0]
+   i = 0
+   while (currrentData != undefined) {
+      i += 1
+      if (currrentData["birdName"] == birdName) {
+         return currrentData
+      }
+      currrentData = currrentData = jsonData["Birds"][i]
+   }
+}
 
 app.listen(8090)
