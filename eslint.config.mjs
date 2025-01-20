7ac54790ec035a777553a916e5f796473147a45e
Jesse Ward <fmfs33@durham.ac.uk>
2025-01-20 17:04:33 +0000

    changed nothing,promise

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {rules: {
+    "no-unused-vars": "warn",
+    "no-undef": "error"
+  }
+  }
 ];
