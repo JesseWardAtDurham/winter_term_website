6c917fafde108d03fd85af32b2b3d9f5cdad0cc9
Jesse Ward <fmfs33@durham.ac.uk>
2025-01-20 17:29:53 +0000

    added jests for get bird checking both robin and goldfinch work

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 const getBird = require("./server")
 
-test('adds 1 + 2 to equal 3', () => {
+test('checks get bird works for robin', () => {
     expect(getBird("Robin")).toStrictEqual({
         "birdName":"Robin",
         "bird_pictures":
@@ -14,5 +14,27 @@ test('adds 1 + 2 to equal 3', () => {
                         "alt_text_for_image":"Picture of a young robin on a tree"
                     }
             ]        
+    });
+  });
+
+test('checks get bird works for goldfinch', () => {
+    expect(getBird("Goldfinch")).toStrictEqual({
+        "birdName":"Goldfinch",
+        "bird_pictures":
+        [
+                    {
+                        "pictureURL":"birdPictures/goldfinch-2766971_640.jpg",
+                        "alt_text_for_image":"goldfinch on a railing"
+                    },
+                    {
+                        "pictureURL":"birdPictures/goldfinch-7228334_640.jpg",
+                        "alt_text_for_image":"goldfinch on a branch"
+                    },
+                    {
+                        "pictureURL":"birdPictures/goldfinch-4232130_640.jpg",
+                        "alt_text_for_image":"goldfinch on a snowy branch"
+                    }
+
+        ]
     });
   });
